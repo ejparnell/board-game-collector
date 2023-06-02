25a513f5be37dd8f10920d443fbda38a8a896c30
ruirosiki <mr.andrew.corbin@icloud.com>
2023-06-02 13:41:11 -0700

    //Complete index and details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+    player_count = models.CharField(max_length=10)
+    description = models.TextField(max_length=250)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,45 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
-games = [
-    {
-        "name": "Spirit Island",
-        "type": "Co-op",
-        "players": "1-6",
-        "description": "Strategy co-op where players take control of elemental spirits to defend their island from foreign invaders",
-        "rating": 8.4,
-    },
-    {
-        "name": "Ark Nova",
-        "type": "Strategy",
-        "players": "1-4",
-        "description": "Strategy game where players plan and build a modern, scientifically managed zoo",
-        "rating": 8.5,
-    },
-]
+# games = [
+#     {
+#         "name": "Spirit Island",
+#         "type": "Co-op",
+#         "players": "1-6",
+#         "description": "Strategy co-op where players take control of elemental spirits to defend their island from foreign invaders",
+#         "rating": 8.4,
+#     },
+#     {
+#         "name": "Ark Nova",
+#         "type": "Strategy",
+#         "players": "1-4",
+#         "description": "Strategy game where players plan and build a modern, scientifically managed zoo",
+#         "rating": 8.5,
+#     },
+# ]
 
-# I want to be able to navigate to separate pages for about and all games using a navbar
-# When I visit the the about page, I want to view some details about the games application
-# When I visit the all page, I want to view a list of all games (index view) that displays each attribute of a game
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def games_index(request):
+    games = Game.objects.all()
+    return render(request, "games/index.html", {"games": games})
+
+
+# respond to request to games_detail, pass in request and game_id
+def games_detail(request, game_id):
+    # create a game variable and assign it to the game objects id
+    game = Game.objects.get(id=game_id)
+    return render(request, "games/details.html", {"game": game})
+
+
+# [x] I want to be able to navigate to separate pages for about and all games using a navbar
+# [x] When I visit the the about page, I want to view some details about the games application
+# [ ] When I visit the all page, I want to view a list of all games (index view) that displays each attribute of a game
